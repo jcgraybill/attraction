5d35725f66e6514b8ef509840270bf21d1c0f50e
jcgraybill <65628360+jcgraybill@users.noreply.github.com>
2022-06-18 11:57:49 -0700

    Add 3rd level and end level

--- level.go
+++ level.go
@@ -70,8 +70,8 @@ var level01 = Level{
 var level02 = Level{
 	label: "level 2",
 	moves: 6,
-	cells: 5,
-	next:  nil,
+	cells: 4,
+	next:  &level03,
 	pieces: []*Piece{
 		{
 			imgsrc: func() *ebiten.Image { return generateTargetImage(color.RGBA{R: 255, G: 0, B: 0, A: 128}) },
@@ -98,6 +98,101 @@ var level02 = Level{
 			imgsrc: func() *ebiten.Image { return generatePostImage() },
 			x:      2,
 			y:      2,
+		},
+	},
+}
+
+var level03 = Level{
+	label: "level 3",
+	moves: 7,
+	cells: 4,
+	next:  &last,
+	pieces: []*Piece{
+		{
+			imgsrc: func() *ebiten.Image { return generateTargetImage(color.RGBA{R: 255, G: 0, B: 0, A: 128}) },
+			x:      1,
+			y:      0,
+			tile:   true,
+			color:  color.RGBA{R: 255, G: 0, B: 0, A: 128},
+		},
+		{
+			imgsrc: func() *ebiten.Image { return generateTargetImage(color.RGBA{R: 0, G: 255, B: 0, A: 128}) },
+			x:      0,
+			y:      0,
+			tile:   true,
+			color:  color.RGBA{R: 0, G: 255, B: 0, A: 128},
+		},
+		{
+			imgsrc:   func() *ebiten.Image { return generateGemImage(color.RGBA{R: 255, G: 0, B: 0, A: 128}) },
+			x:        1,
+			y:        1,
+			moveable: true,
+			color:    color.RGBA{R: 255, G: 0, B: 0, A: 128},
+		},
+		{
+			imgsrc:   func() *ebiten.Image { return generateGemImage(color.RGBA{R: 0, G: 255, B: 0, A: 128}) },
+			x:        0,
+			y:        1,
+			moveable: true,
+			color:    color.RGBA{R: 0, G: 255, B: 0, A: 128},
+		},
+
+		{
+			imgsrc:   func() *ebiten.Image { return generateMagnetImage() },
+			x:        3,
+			y:        0,
+			moveable: true,
+			magnetic: true,
+		},
+		{
+			imgsrc:   func() *ebiten.Image { return generateMagnetImage() },
+			x:        3,
+			y:        1,
+			moveable: true,
+			magnetic: true,
+		},
+		{
+			imgsrc:   func() *ebiten.Image { return generateBlockImage() },
+			x:        2,
+			y:        2,
+			moveable: true,
+		},
+	},
+}
+
+var last = Level{
+	label: "thanks for playing",
+	cells: 2,
+	moves: 0,
+	next:  nil,
+	pieces: []*Piece{
+		{
+			imgsrc:   func() *ebiten.Image { return generateGemImage(color.RGBA{R: 255, G: 0, B: 0, A: 128}) },
+			x:        0,
+			y:        0,
+			moveable: true,
+			color:    color.RGBA{R: 255, G: 0, B: 0, A: 128},
+		},
+		{
+			imgsrc:   func() *ebiten.Image { return generateGemImage(color.RGBA{R: 0, G: 255, B: 0, A: 128}) },
+			x:        1,
+			y:        0,
+			moveable: true,
+			color:    color.RGBA{R: 255, G: 0, B: 0, A: 128},
+		},
+		{
+			imgsrc:   func() *ebiten.Image { return generateGemImage(color.RGBA{R: 0, G: 0, B: 255, A: 128}) },
+			x:        0,
+			y:        1,
+			moveable: true,
+			color:    color.RGBA{R: 255, G: 0, B: 0, A: 128},
+		},
+		{
+			imgsrc:   func() *ebiten.Image { return generateGemImage(color.RGBA{R: 255, G: 0, B: 255, A: 128}) },
+			x:        1,
+			y:        1,
+			moveable: true,
+			color:    color.RGBA{R: 255, G: 0, B: 0, A: 128},
 		},
 	},
 }
