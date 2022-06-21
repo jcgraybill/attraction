39b3bc9260b2026627d292d86286badc604c440c
jcgraybill <65628360+jcgraybill@users.noreply.github.com>
2022-06-20 22:20:06 -0700

    help text

--- help.go
+++ help.go
@@ -8,14 +8,27 @@ import (
 var helpSplash *ebiten.Image
 
 func generateHelpSplashScreen() *ebiten.Image {
-	//	var topics [6]string
+	var topics [6]string
 	//	var images [6]*ebiten.Image
+
+	topics = [6]string{
+		"move colored circles into\nmatching slots.",
+		"use arrow keys to pulse\nan electromagnet.",
+		"magnetic pieces will move\none cell in that direction.",
+		"moving pieces push other\nmoveable pieces.",
+		"you can push hollow\nblocks.",
+		"black squares don't move.",
+	}
 
 	splash := ebiten.NewImage(w, h)
 	splash.Fill(bg)
 
-	message := "this will be the help screen"
-	text.Draw(splash, message, *regular, w/2-text.BoundString(*regular, message).Dx()/2, 10+text.BoundString(*regular, message).Dy(), fg)
+	for i, topic := range topics {
+		text.Draw(splash, topic, *regular, 100, h*i/(len(topics)+1)+h/(len(topics)+1), fg)
+	}
+
+	message := "'esc': continue"
+	text.Draw(splash, message, *regular, w/2-text.BoundString(*regular, message).Dx()/2, h-10, fg)
 
 	return splash
 }
